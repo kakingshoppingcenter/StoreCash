0758c04d773a5319ad20d188543670b70f8d4582
kakingshoppingcenter <kakingshoppingcenterid@gmail.com>
2026-07-29 15:05:36 +0800

    feat: load protected administrator deletion controls

--- config.js
+++ config.js
@@ -32,3 +32,20 @@ window.KSC_CONFIG = Object.freeze({
     applyFavicon();
   }
 })();
+
+(function loadProtectedAdministratorDeletion() {
+  function loadExtension() {
+    if (document.querySelector('script[data-ksc-admin-delete]')) return;
+    const script = document.createElement('script');
+    script.src = './admin-delete.js?v=20260729-1458';
+    script.dataset.kscAdminDelete = 'true';
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  if (document.readyState === 'complete') {
+    window.setTimeout(loadExtension, 0);
+  } else {
+    window.addEventListener('load', loadExtension, { once: true });
+  }
+})();
